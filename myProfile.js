461411773d404494c41c6e42f2a1d0ac0139e4ee
subhamBakshi <subhambakshi12@gmail.com>
2020-07-01 11:04:04 +0530

    First Project

---
+++ myProfile.js
@@ -0,0 +1,7 @@
+// submitBtn.addEventListener("click", function () {
+//     alert("Thank You!");
+// })
+
+function myFunction() {
+    document.getElementById("submitBtn").innerHTML = alert("Thank You!");
+}
