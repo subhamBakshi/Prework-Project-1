21726c3cbda87584f84f12544f19050a17a66781
Subham Bakshi <67575802+subhamBakshi@users.noreply.github.com>
2020-07-01 11:17:49 +0530

    Update myProfile.js

--- myProfile.js
+++ myProfile.js
@@ -1,7 +1,1 @@
-// submitBtn.addEventListener("click", function () {
-//     alert("Thank You!");
-// })
-
-function myFunction() {
-    document.getElementById("submitBtn").innerHTML = alert("Thank You!");
-}
+//nothing here.
